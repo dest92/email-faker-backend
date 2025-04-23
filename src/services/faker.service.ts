fb82c6b31a812f13a2e2179df15a82b34241d628
dest92 <acebalmatias2019@gmail.com>
2025-04-23 13:07:42 -0300

    avatar by gender

--- src/services/faker.service.ts
+++ src/services/faker.service.ts
@@ -1,5 +1,5 @@
-import { faker } from '@faker-js/faker/locale/es';
-import { UserProfile } from '../interfaces/mail.interface';
+import { faker } from "@faker-js/faker/locale/es";
+import { UserProfile } from "../interfaces/mail.interface";
 
 class FakerService {
   /**
@@ -8,28 +8,36 @@ class FakerService {
    */
   generateUserProfile(): UserProfile {
     const gender = faker.person.sex();
-    const firstName = faker.person.firstName(gender === 'male' ? 'male' : 'female');
+    const firstName = faker.person.firstName(
+      gender === "male" ? "male" : "female"
+    );
     const lastName = faker.person.lastName();
-    const birthdate = faker.date.birthdate({ min: 18, max: 65, mode: 'age' });
-    
+    const birthdate = faker.date.birthdate({ min: 18, max: 65, mode: "age" });
+
+    // Generar avatar específico según el género usando personPortrait
+    const avatar = faker.image.personPortrait({
+      sex: gender === "male" ? "male" : "female",
+      size: 512
+    });
+
     return {
       firstName,
       lastName,
       fullName: `${firstName} ${lastName}`,
       gender,
       age: new Date().getFullYear() - birthdate.getFullYear(),
-      birthdate: birthdate.toISOString().split('T')[0],
-      avatar: faker.image.avatar(),
+      birthdate: birthdate.toISOString().split("T")[0],
+      avatar: avatar,
       address: {
         street: faker.location.streetAddress(),
         city: faker.location.city(),
         state: faker.location.state(),
         zipCode: faker.location.zipCode(),
-        country: faker.location.country()
+        country: faker.location.country(),
       },
       phone: faker.phone.number(),
       occupation: faker.person.jobTitle(),
-      company: faker.company.name()
+      company: faker.company.name(),
     };
   }
 }
